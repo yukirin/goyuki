022949ce5e6edaff02781a80d4def0f99d0d4011
yukirin <standupdown@gmail.com>
2016-02-02 19:03:40 +0900

    Add help message

--- command/get.go
+++ command/get.go
@@ -4,22 +4,28 @@ import (
 	"strings"
 )
 
+// GetCommand is a Command that get test case
 type GetCommand struct {
 	Meta
 }
 
+// Run get test case
 func (c *GetCommand) Run(args []string) int {
 	// Write your code here
 
 	return 0
 }
 
+// Synopsis is a one-line, short synopsis of the command.
 func (c *GetCommand) Synopsis() string {
-	return ""
+	return "Get test case"
 }
 
+// Help is a long-form help text
 func (c *GetCommand) Help() string {
 	helpText := `
+Usage:
+	goyuki get problem_no
 
 `
 	return strings.TrimSpace(helpText)
--- command/run.go
+++ command/run.go
@@ -4,22 +4,28 @@ import (
 	"strings"
 )
 
+// RunCommand is a Command that run the test
 type RunCommand struct {
 	Meta
 }
 
+// Run run the test
 func (c *RunCommand) Run(args []string) int {
 	// Write your code here
 
 	return 0
 }
 
+// Synopsis is a one-line, short synopsis of the command.
 func (c *RunCommand) Synopsis() string {
-	return ""
+	return "Run the test"
 }
 
+// Help is a long-form help text
 func (c *RunCommand) Help() string {
 	helpText := `
+Usage:
+	goyuki run problem_no exec_file
 
 `
 	return strings.TrimSpace(helpText)
