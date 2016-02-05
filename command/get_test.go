8106861cbcc0cd29d5662756bb12b6e5c03099e3
yukirin <standupdown@gmail.com>
2016-02-05 17:13:31 +0900

    Add TestGetCommand

--- command/get_test.go
+++ command/get_test.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/mitchellh/cli"
@@ -8,4 +10,40 @@ import (
 
 func TestGetCommand_implement(t *testing.T) {
 	var _ cli.Command = &GetCommand{}
+}
+
+func TestGetCommand(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &GetCommand{
+		Meta: Meta{
+			UI: ui,
+		},
+	}
+
+	tmpDir, err := ioutil.TempDir("", "test-get-command")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	prev, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	backFunc, err := tmpChdir(tmpDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer backFunc()
+
+	args := []string{"337"}
+
+	if code := c.Run(args); code != ExitCodeOK {
+		t.Fatalf("bad status code = %v; want %v\n\n%s", code, ExitCodeOK, ui.ErrorWriter.String())
+	}
+
+	if !equalFiles(prev+"/testdata/337", tmpDir+"/337") {
+		t.Errorf("failed download testcase; problem no %s\n", args[0])
+	}
 }
