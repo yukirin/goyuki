057f0af324f7103e2d2eb4a05ad4b4387b22a161
yukirin <standupdown@gmail.com>
2016-02-05 15:27:32 +0900

    Update Diff validater bytes.Equal -> bufio.NewScanner

--- command/validater.go
+++ command/validater.go
@@ -1,6 +1,9 @@
 package command
 
-import "bytes"
+import (
+	"bufio"
+	"bytes"
+)
 
 // Validater is the interface that wraps the Validate method
 type Validater interface {
@@ -13,7 +16,20 @@ type DiffValidater struct {
 
 // Validate is verifies the exact match
 func (d *DiffValidater) Validate(actual, expected []byte) bool {
-	return bytes.Equal(actual, expected)
+	asc := bufio.NewScanner(bytes.NewReader(actual))
+	esc := bufio.NewScanner(bytes.NewReader(expected))
+
+	next1, next2 := asc.Scan(), esc.Scan()
+	for ; next1 && next2; next1, next2 = asc.Scan(), esc.Scan() {
+		if asc.Text() != esc.Text() {
+			return false
+		}
+	}
+
+	if asc.Err() == nil && esc.Err() == nil && next1 == next2 {
+		return true
+	}
+	return false
 }
 
 // Validaters is map of available validater
