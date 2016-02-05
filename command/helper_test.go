df9b14a7e1a7ed80db5f70869f88aa798716e688
yukirin <standupdown@gmail.com>
2016-02-05 18:16:16 +0900

    Remove equalFiles

--- command/helper_test.go
+++ command/helper_test.go
@@ -1,11 +1,6 @@
 package command
 
-import (
-	"os"
-	"path/filepath"
-	"reflect"
-	"strings"
-)
+import "os"
 
 // setEnv set enviromental variables and return restore function.
 func setEnv(key, val string) func() {
@@ -31,31 +26,4 @@ func tmpChdir(dir string) (func(), error) {
 	return func() {
 		os.Chdir(currentDir)
 	}, nil
-}
-
-func equalFiles(dir1, dir2 string) bool {
-	list1 := make(map[string]struct{})
-	list2 := make(map[string]struct{})
-
-	list, dir := list1, dir1
-	f := filepath.WalkFunc(func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		relPath := strings.TrimPrefix(path, dir)
-		list[relPath] = struct{}{}
-
-		return nil
-	})
-
-	if err := filepath.Walk(dir1, f); err != nil {
-		return false
-	}
-
-	list, dir = list2, dir2
-	if err := filepath.Walk(dir2, f); err != nil {
-		return false
-	}
-
-	return reflect.DeepEqual(list1, list2)
 }
