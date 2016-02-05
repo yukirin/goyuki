e2221c22306e9b4ee53728b3f388f04c78accf88
yukirin <standupdown@gmail.com>
2016-02-05 16:24:52 +0900

    Add DiffValidater test

--- command/validater_test.go
+++ command/validater_test.go
@@ -3,5 +3,22 @@ package command
 import "testing"
 
 func TestDiffValidater(t *testing.T) {
-	t.Errorf("error")
+	testCases := []struct {
+		b1     []byte
+		b2     []byte
+		result bool
+	}{
+		{[]byte(""), []byte(""), true},
+		{[]byte(""), []byte("foo\nbar"), false},
+		{[]byte("foo\nbar"), []byte("foo\nbar\n"), true},
+		{[]byte("foo\nbar\nfuga\n"), []byte("foo\nbar\nfuga\n"), true},
+	}
+
+	validater := DiffValidater{}
+	for _, testCase := range testCases {
+		result := validater.Validate(testCase.b1, testCase.b2)
+		if result != testCase.result {
+			t.Errorf("Validate(%v, %v) = %v; want %v", testCase.b1, testCase.b2, result, testCase.result)
+		}
+	}
 }
