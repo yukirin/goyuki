7ba0d323053cca3aa9a65b32af87a5cd8ce24db1
yukirin <standupdown@gmail.com>
2016-02-06 16:24:21 +0900

    Add TestFloatValidater

--- command/validater_test.go
+++ command/validater_test.go
@@ -22,3 +22,24 @@ func TestDiffValidater(t *testing.T) {
 		}
 	}
 }
+
+func TestFloatValidater(t *testing.T) {
+	testCases := []struct {
+		b1     []byte
+		b2     []byte
+		result bool
+	}{
+		{[]byte(""), []byte(""), true},
+		{[]byte(""), []byte("1.11"), false},
+		{[]byte("1.23 2.23\n3.23"), []byte("1.23 2.23\n3.23"), true},
+		{[]byte("1e6"), []byte("1000000"), true},
+	}
+
+	validater := FloatValidater{}
+	for _, testCase := range testCases {
+		result := validater.Validate(testCase.b1, testCase.b2)
+		if result != testCase.result {
+			t.Errorf("Validate(%v, %v) = %v; want %v", testCase.b1, testCase.b2, result, testCase.result)
+		}
+	}
+}
